6becd8b6458194c9c0a2d600782de101ee297e4a
Obee88 <obee88@gmail.com>
2019-05-25 21:07:23 +0200

    Updates

---
+++ precache-manifest.acd1d3bd4d84a8e7f0a40a9399c55ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac10562f36b974aa16c93e94f3aab74f",
+    "url": "/minesweeper-react/index.html"
+  },
+  {
+    "revision": "57b8ade25090f4897860",
+    "url": "/minesweeper-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "922bae66f77f73789206",
+    "url": "/minesweeper-react/static/css/main.1de22707.chunk.css"
+  },
+  {
+    "revision": "57b8ade25090f4897860",
+    "url": "/minesweeper-react/static/js/2.20cd22e9.chunk.js"
+  },
+  {
+    "revision": "922bae66f77f73789206",
+    "url": "/minesweeper-react/static/js/main.624b17bd.chunk.js"
+  },
+  {
+    "revision": "9eba16aae770b4b8a2c9",
+    "url": "/minesweeper-react/static/js/runtime~main.4d91ba56.js"
+  }
+]);
